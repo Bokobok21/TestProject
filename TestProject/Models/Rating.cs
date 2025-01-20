25c50d3209383dfc140df17846ba6353d16d2da6
bbozh <bbozh@LENOVOLEGION>
2025-01-20 12:23:14 +0200

    Add project files.

---
+++ TestProject/Models/Rating.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProject.Models
+{
+    public class Rating
+    {
+        public Trips Trips { get; set; }
+        // public string Tourist { get; set; }
+        public double Score { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ TestProject/Models/Trips.cs
@@ -0,0 +1,13 @@
+﻿namespace TestProject.Models
+{
+    public class Trips
+    {
+     //   public string Driver { get; set; }
+        public string StartPosition { get; set; }
+        public string EndPosition { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int Price { get; set; }
+        public Rating Rating { get; set; }
+        public int FreeSeats { get; set; }
+    }
+}
