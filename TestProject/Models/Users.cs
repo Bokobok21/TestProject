10551d0d4a9bae7566457e353bfdd3851e37bc77
Bokobok21 <bbozhanov06@gmail.com>
2025-01-24 13:31:56 +0200

    fixed all my entities and relations

--- TestProject/Models/Users.cs
+++ TestProject/Models/Users.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using System.ComponentModel.DataAnnotations;
 
 namespace TestProject.Models
 {
     public class Users : IdentityUser
     {
         [Required]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
         [Required]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
         public string Position { get; set; } = string.Empty;
 
         public List<Trips>? Trips { get; set; }
