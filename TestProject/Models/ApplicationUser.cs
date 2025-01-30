1e2ff3ab862e975bba6568eae68eb0b03519d58f
Bokobok21 <bbozhanov06@gmail.com>
2025-01-30 08:10:10 +0200

    comments

--- TestProject/Models/ApplicationUser.cs
+++ TestProject/Models/ApplicationUser.cs
@@ -20,3 +20,7 @@ namespace TestProject.Models
     }
 }
 //i have four tabbles/entitie/models iin asp net core i have applicationuser and trip and review and request and i have admin/moderator/tourist roles. only drivers can make a trip and when you first register you become tourist but you can apply if you want to be a driver and there youe enter the info for the cars you have and their plate numbers. so this probably mmeans i need to make another table cars. the drivers has the option to add more cars later if he wants. 
+// kak shte se integrira service/Iservice, viewModel, partiaView
+// izpolzvai DisplayFor and EditFor
+// v0 i codepen
+
